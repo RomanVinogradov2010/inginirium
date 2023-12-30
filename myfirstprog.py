a85cd1dc1d8f2dea6664bec1fdc217b7454ec398
RomanVinogradov2010 <155223863+RomanVinogradov2010@users.noreply.github.com>
2023-12-30 14:40:03 +0600

    Программа для определения координат

---
+++ myfirstprog.py
@@ -0,0 +1,10 @@
+def my_function(х, y): 
+    if х > 0 and y > 0: 
+        print("1") 
+    elif х < 0 and y > 0: 
+        print("2") 
+    elif х < 0 and y < 0: 
+        print("3")
+    elif х > 0 and y < 0: 
+        print("4") 
+my_function (5,5)
